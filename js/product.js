b85f99c7f6e2988856ea8042ba378db5b371a74d
mazur <grzaneczka@gmail.com>
2019-04-17 09:11:32 +0200

    added videos in product.html but there is an issue with aspect ratio on full screen

--- js/product.js
+++ js/product.js
@@ -14,13 +14,15 @@ var dynamicContent = getParameterByName('id');
 //the id in URL comes from dynamically set "href" in blog.html through blog.js
 //the id set in this URL comes from fetched json file in blog.js
 
-var paintings = [];
+let paintings = [];
+let videosArray = [];
 
 function getAllPaintings() {
   fetch('http://dashboard.siqiuli.com/?rest_route=/wp/v2/art/' + dynamicContent)
     .then(res => res.json())
     .then(showPaintings)
-    .then(placeSize);
+    .then(placeSize)
+    .then(muteVideos);
 }
 
 function showPaintings(data) {
@@ -31,6 +33,7 @@ function showPaintings(data) {
   // let clone = template.cloneNode(true);
   let images = data.acf.art_piece.images;
   let images2 = data.acf.art_piece.images2;
+  let videos = data.acf.art_piece.videos;
   let sizeJson = data.acf.art_piece.size;
   // let descJson = data.acf.art_piece.description;
   let titleJson =
@@ -48,17 +51,32 @@ function showPaintings(data) {
     for (key in imgs) {
       ///looping through all keys in this product(acf= advanced custom fields)
       if (key) {
-        let clone = template.cloneNode(true);
+        // let clone = template.cloneNode(true);
         let imageTemp = key;
         let photo = data.acf.art_piece.images[imageTemp];
         if (photo) {
           paintings.push(photo);
-          list.appendChild(clone);
         }
       }
     }
   }
+
+  function putVideosInArray(vids) {
+    for (key in vids) {
+      ///looping through all keys in this product(acf= advanced custom fields)
+      if (key) {
+        // let clone = template.cloneNode(true);
+        let vidTemp = key;
+        let video = data.acf.art_piece.videos[vidTemp];
+        if (video) {
+          videosArray.push(video);
+        }
+      }
+    }
+  }
+
   putImagesInArray(images);
+  putVideosInArray(videos);
   putImagesInArray(images2);
   fillColumns();
 }
@@ -66,7 +84,6 @@ function showPaintings(data) {
 getAllPaintings();
 
 function fillColumns() {
-  console.log(paintings.length);
   for (i = 0; i < paintings.length; i = i + 2) {
     var img = document.createElement('img');
     img.src = paintings[i];
@@ -79,11 +96,39 @@ function fillColumns() {
     }
   }
 
+  for (i = 0; i < videosArray.length; i = i + 2) {
+    let vidEl = document.createElement('video');
+    let srcEl = document.createElement('source');
+
+    if (videosArray[i]) {
+      srcEl.src = videosArray[i];
+      srcEl.setAttribute('type', 'video/mp4');
+      vidEl.setAttribute('controls', true);
+      vidEl.setAttribute('autoplay', true);
+
+      vidEl.appendChild(srcEl);
+      document.querySelector('#leftImg').appendChild(vidEl);
+    }
+  }
+
+  for (i = 1; i < videosArray.length; i = i + 2) {
+    let vidEl = document.createElement('video');
+    let srcEl = document.createElement('source');
+
+    if (videosArray[i]) {
+      srcEl.src = videosArray[i];
+      srcEl.setAttribute('type', 'video/mp4');
+      vidEl.setAttribute('controls', 'true');
+      vidEl.setAttribute('autoplay', 'on');
+      vidEl.appendChild(srcEl);
+      document.querySelector('#rightImg').appendChild(vidEl);
+    }
+  }
+
   for (i = 1; i < paintings.length; i = i + 2) {
     var img = document.createElement('img');
     img.src = paintings[i];
     if (i == 1 || i == 3 || i == 5) {
-      console.log('substracted from right to left' + i);
       document.querySelector('#leftImg').appendChild(img);
     }
     document.querySelector('#rightImg').appendChild(img);
@@ -93,24 +138,10 @@ function fillColumns() {
 function placeSize() {
   if (!isMobile) {
     let tempLeft = $('#main-image').offset().left;
-    let docLeft = $(document).offset().left;
-
     $('#size').css('left', tempLeft / 2);
   }
 }
 
-// function mobile() {
-//   if (isMobile) {
-//     let tempHeight;
-//     let tempTop;
-//     var $img = $('#main-image');
-
-//     $img.on('load', function() {
-//       tempHeight = $img.height();
-//       tempTop = $(this).offset().top;
-//     });
-//     console.log(tempHeight);
-
-//     $('.parent').css('margin-top', tempTop);
-//   }
-// }
+function muteVideos() {
+  $('video').prop('volume', 0.5);
+}
